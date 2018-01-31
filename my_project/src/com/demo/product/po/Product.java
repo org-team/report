a4adf8f6cf695725e81e6d96058abd3a6ac378da
kouyulong <1270179678@qq.com>
2018-02-01 07:53:48 +0800

    更新

---
+++ my_project/src/com/demo/product/po/Product.java
@@ -0,0 +1,12 @@
+package com.demo.product.po;
+
+import com.utils.BaseModel;
+
+/**
+ * 封装与商品相关的信息
+ * @author Administrator
+ */
+public class Product extends BaseModel{
+
+//	private String 
+}
--- my_project/src/com/utils/SetSqlId.java
+++ my_project/src/com/utils/SetSqlId.java
@@ -12,4 +12,5 @@ public class SetSqlId {
 		String uuid = UUID.randomUUID().toString().replaceAll("-", "");
 		return uuid;
 	}
+	
 }
